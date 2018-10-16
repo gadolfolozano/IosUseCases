d8798396163f46260cf012ac1bdf8bcfe2f5272d
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2018-10-15 22:15:12 -0500

    Initial Commit

---
+++ IosUseCases/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IosUseCases
+//
+//  Created by Adolfo Lozano Mendez on 15/10/18.
+//  Copyright © 2018 Adolfo Lozano Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
